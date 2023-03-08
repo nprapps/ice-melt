444256120e58294a7f902afb743c1213460e3e8d
Alyson Hurt <alykat@gmail.com>
2023-03-07 23:40:17 -0500

    prototype quiz design/interaction #92

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,44 @@ if (isOne) {
   document.body.classList.add("nprone");
 }
 
+// quiz setup
+var onQuizButtonClicked = function(evt) {
+  console.log(this, evt);
+
+  // flag that this question has been answered
+  var qParent = this.parentNode.parentNode.parentNode;
+  qParent.dataset.answered = true;
+  qParent.dataset.correct = this.dataset.status;
+
+  // deactivate the buttons
+  var qBtns = qParent.querySelectorAll("button");
+  qBtns.forEach(function(btn) {
+    btn.removeEventListener("click", onQuizButtonClicked);
+    btn.disabled = true;
+  });
+
+  // flag if you got this right or wrong
+  switch(this.dataset.status) {
+    case "true":
+      this.innerHTML += ` <i>Right!</i>`;
+      break;
+    case "false":
+      this.innerHTML += ` <i>Wrong!</i>`;
+      break;
+  }
+
+  // smoothscroll to the next slide?
+  var nextSlide = document.getElementById(qParent.nextElementSibling.id);
+  nextSlide.scrollIntoView({ behavior: "smooth" });
+}
+
+var quizSlides = document.querySelectorAll(".quiz");
+if (quizSlides.length > 0) {
+  var qButtons = document.querySelectorAll(".quiz button");
+  qButtons.forEach(function(qb) {
+    qb.addEventListener("click", onQuizButtonClicked);
+  });
+}
 
 var active = null;
 
