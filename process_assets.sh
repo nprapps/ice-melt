22fd38d7c409b823e0aef25c8f1e04b303f81f66
Alyson Hurt <alykat@gmail.com>
2023-03-14 09:53:52 -0400

    add audio processing to asset resize script

--- process_assets.sh
+++ process_assets.sh
@@ -4,6 +4,8 @@
 # if you want to update an existing file, delete the output first
 # instructions for how to use this are in the README
 
+# best practices: https://github.com/nprapps/bestpractices/blob/master/assets.md
+
 # set to "error" to suppress logs
 ffmpeg_log="error"
 
@@ -12,38 +14,37 @@ ffmpeg_log="error"
 cd src/assets/synced/images
 mkdir -p resized
 for img in *.jpg; do
-  echo "Processing $img..."
-  # if [ ! -f $img ]; then
+  if [ ! -f resized/$img ]; then
+    echo "Processing $img..."
     magick convert $img -resize 1600x1200\> -quality 75 -strip -sampling-factor 4:2:0 -define jpeg:dct-method=float -interlace Plane resized/$img;
-  # fi
+  fi
 done
 for img in *.png; do
-  echo "Processing $img..."
-  # if [ ! -f $img ]; then
+  if [ ! -f resized/$img ]; then
+    echo "Processing $img..."
     magick convert $img -resize 1600x1200\> \
       -define png:compression-filter=5 \
       -define png:compression-level=9 \
       resized/$img;
-  # fi
+  fi
 done
 for img in *.jpeg; do
-  echo "Processing $img..."
-  # if [ ! -f $img ]; then
+  if [ ! -f resized/$img ]; then
+    echo "Processing $img..."
     magick convert $img -resize 1600x1200\> \
       -define png:compression-filter=5 \
       -define png:compression-level=9 \
       resized/$img;
-  # fi
+  fi
 done
 
 # handle silenced video
 cd ../video
 mkdir -p resized
 for video in *.mp4; do
-  echo "Processing $video..."
-
   # create the videos
-  # if [ ! -f $video ]; then
+  if [ ! -f resized/$video ]; then
+    echo "Processing $video..."
     ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
     -i $video \
     -an \
@@ -55,17 +56,29 @@ for video in *.mp4; do
     -vf scale=1500:-2 \
     -movflags +faststart \
     resized/$video;
-  # fi
+  fi
 
   # create posters
-  # if [ ! -f $video.jpg ]; then
+  if [ ! -f resized/$video.jpg ]; then
+    echo "Processing poster image for $video..."
     ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
     -i $video \
     -vf scale=1600:-2 \
     -qscale:v 4 \
     -frames:v 1 \
     resized/$video.jpg;
-  # fi
+  fi
+done
+
+# convert audio
+cd ../audio
+mkdir -p resized
+for audio in *.mp3; do
+  # create the audio
+  if [ ! -f resized/$audio ]; then
+    echo "Processing $audio..."
+    lame -m s -b 96 $audio resized/$audio;
+  fi
 done
 
 # convert satellite images
