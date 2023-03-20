b0b092be590f93849f24e0f880e81e0184430c01
Jacob Fenton <@npr.org>
2023-03-20 14:33:55 -0700

    remove most filters

--- src/js/map.js
+++ src/js/map.js
@@ -134,13 +134,13 @@ function initialize_map() {
 	  grid = svg.append("path")
 	    .attr("stroke-width","0.5px")
 	    .attr("stroke","#eee")
-	    .attr("filter", "url(#pencilTexture4");
+	    //.attr("filter", "url(#pencilTexture4");
 	  
 	  feature = svg.append("path")
 	  	.attr("stroke","#000")
 	    .attr("stroke-width", "3px")
 	    .attr("fill","#eeeeee")
-	    .attr("filter", "url(#pencilTexture3");
+	    //.attr("filter", "url(#pencilTexture3");
 
 
 	  let topology = mapdata;
